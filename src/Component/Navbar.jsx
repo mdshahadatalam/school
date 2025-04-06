2097cb14df44d828994262f3d7507838b5083e0c
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-04-05 22:05:36 -0700

    navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {IoIosSearch} from "react-icons/io";
 import logo from '../assets/navbar-brand sec-.png'
+import { CiMenuFries } from 'react-icons/ci';
 
 export const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -29,24 +30,28 @@ export const Navbar = () => {
     
 
     <div>
-          <button className='font-bold text-[14px] leading-[157%] tracking-[0.01em] text-right text-[var(--primary-color)] mx-3'>login</button>
+          <button className='font-bold text-[18px] leading-[157%] tracking-[0.01em] text-right text-[var(--primary-color)] mx-3 sm:none'>login</button>
     </div>
 
       <div>
-      <button className='rounded-[5px] px-[25px] py-[15px] w-[137px] h-[52px] bg-[var(--primary-color)] font-bold text-[14px] leading-[157%] tracking-[0.01em] text-[var(--light-background-color)] shadow-md'>JOIN US</button>
+      <button className='rounded-[5px] px-[25px] py-[15px] w-[137px] h-[52px] bg-[var(--primary-color)] font-bold text-[14px] leading-[157%] tracking-[0.01em] text-[var(--light-background-color)] shadow-md     transition-all duration-300 hover:bg-[var(--primary-color)] hover:scale-105 '>JOIN US</button>
       </div>
+
+                 <CiMenuFries
+                    className="text-[1.6rem] text-[#424242] cursor-pointer md:hidden flex"
+                    onClick={() => setIsMenuOpen(!isMenuOpen)}/>
 </div>
 
 {/* mobile sidebar */}
 <aside
-    className={` ${isMenuOpen ? "translate-x-0 opacity-100 z-20" : "translate-x-[200px] opacity-0 z-[-1]"} md:hidden bg-[#3B9DF8] p-4 text-center absolute top-[60px] right-0 w-full sm:w-[300px] rounded-md transition-all duration-300`}>
+    className={` ${isMenuOpen ? "translate-y-0 opacity-100 z-20" : "translate-y-[200px] opacity-0 z-[-1]"} md:hidden bg-black p-4 text-center absolute top-[60px] right-0 w-full sm:w-[300px] rounded-md transition-all duration-300`}>
 
     <div className="w-full relative mb-5">
         <input
-            className="py-1.5 pr-4 pl-12 w-full rounded-full outline-none focus:border-[#3B9DF8]"
+            className="py-1.5 pr-4 pl-12 w-full rounded-full outline-none focus:border-white"
             placeholder="Search..."/>
         <IoIosSearch
-            className="absolute top-[9px] left-5 text-[#424242] text-[1.3rem]"/>
+            className="absolute top-[9px] left-5 text-white text-[1.3rem]"/>
     </div>
 
     <ul className="items-center gap-[20px] text-[1rem] text-white flex flex-col">
