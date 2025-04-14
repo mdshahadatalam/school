0ede7b9cc4ed247243b14afa3efe812d41e8b81c
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-04-14 10:50:40 -0700

    more

--- src/Component/Subcripe.jsx
+++ src/Component/Subcripe.jsx
@@ -11,6 +11,24 @@ export const Subcripe = () => {
                    <p className='font-normal text-[14px] leading-[143%] tracking-[0.01em] text-[#737373]'>Problems trying to resolve the conflict between <br />
                    the two major realms of Classical physics: Newtonian mechanics </p>
               </div>
+
+
+              <form action="" className="flex justify-center px-4">
+                <div className="flex flex-col sm:flex-row sm:items-center btn-group w-full max-w-[640px] py-5">
+                    <input
+                       className="border border-[#e6e6e6] rounded-[5px] sm:rounded-r-none w-full h-[50px] bg-[#f9f9f9]                  outline-none px-4"
+                       type="email"
+                       name="email"
+                       id="email"
+                       placeholder="Your Email"
+                     />
+             <button
+               className="border border-[#e6e6e6] sm:border-l-0 rounded-[5px] sm:rounded-l-none w-full sm:w-[117px] h-[50px] bg-[#96BB7C] font-normal text-sm leading-[200%] tracking-[0.01em] font-sans text-white">
+                    Subscribe
+              </button>
+            </div>
+          </form>
+
            </div>
        </section>
     </>
